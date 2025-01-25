9420525e47bf829108f84fcac869327028a6cfb0
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-25 14:55:44 +0200

    Add CI/CD workflow for remote deployment and update Login header

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -30,7 +30,7 @@ const Login: React.FC = () => {
 
 	return (
 		<div>
-			<h2>Login</h2>
+			<h2>Login (CI-CD)</h2>
 			{error && <p style={{ color: 'red' }}>{error}</p>}
 			<input type='text' placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} />
 			<input
