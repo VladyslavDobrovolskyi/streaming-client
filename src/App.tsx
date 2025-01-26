5e94b07f87449d3818b1c0b76786e4a9d1924e83
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-26 17:04:23 +0200

    Reorganize component structure: move Provider to main entry point and wrap App component for proper Redux context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Room from './pages/Room'
 import Main from './pages/Main'
-import { Provider } from 'react-redux'
-import store from './redux/store'
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Provider store={store}>
-				<Routes>
-					<Route path='/room/:id' element={<Room />} />
-					<Route path='/' element={<Main />} />
-				</Routes>
-			</Provider>
+			<Routes>
+				<Route path='/room/:id' element={<Room />} />
+				<Route path='/' element={<Main />} />
+			</Routes>
 		</BrowserRouter>
 	)
 }
