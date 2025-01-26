bfdc2a5bb8b419f9a838c7d241e1d1b7d7453d6d
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-26 18:35:16 +0200

    Refactor LoginForm component: update import path for authSlice and add Link component for navigation

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import DOMPurify from 'dompurify'
 import { validatePasswordLength } from '../utils/validation'
-import { login } from '../features/auth/authSlice'
+import { login } from '../features/auth/'
+import { Link } from 'react-router-dom'
 import styles from './AuthForm.module.css'
 
 const LoginForm = () => {
