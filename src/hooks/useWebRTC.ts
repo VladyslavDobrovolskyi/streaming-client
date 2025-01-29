d885b5fc45a817dd9993e7fdd7af88a64284497b
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-29 14:54:24 +0200

    Update WebRTC configuration to use a specific STUN server and enable unified-plan SDP semantics

--- src/hooks/useWebRTC.ts
+++ src/hooks/useWebRTC.ts
@@ -28,10 +28,19 @@ export default function useWebRTC(roomID: string) {
 
 	const iceRetryTimeout = 5000 // 2 seconds timeout
 
+	const configuration = {
+		sdpSemantics: 'unified-plan', // Новый стандарт WebRTC
+		iceServers: [
+			{
+				urls: 'stun:92.112.180.234:3478', // Адрес TURN сервера
+				credential: '9S2T4U0N5', // Ваш цифровой ключ, использованный в конфиге
+			},
+		],
+		iceCandidatePoolSize: 2,
+	}
+
 	const createPeerConnection = (peerID: string) => {
-		const connection = new RTCPeerConnection({
-			iceServers: [{ urls: 'stun:stun2.1.google.com:19302' }],
-		})
+		const connection = new RTCPeerConnection(configuration)
 
 		connection.onicecandidate = event => {
 			if (event.candidate) {
