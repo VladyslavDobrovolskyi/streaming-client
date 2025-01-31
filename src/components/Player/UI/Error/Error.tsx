47767b62a895d0329da8c15db87dd5c191273b43
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-31 10:11:48 +0200

    Add react-transition-group dependency and refactor components for improved error handling and UI transitions

--- src/components/Player/UI/Error/Error.tsx
+++ src/components/Player/UI/Error/Error.tsx
@@ -1,26 +1,28 @@
-import { memo } from 'react';
+import { memo } from 'react'
 
-import { ReactComponent as ReloadIcon } from 'icons/reload.svg';
-import './Error.css';
+import ReloadIcon from 'icons/reload.svg'
+import './Error.css'
 
 interface ErrorProps {
-  error: MediaError | null;
+	error: MediaError | null
 }
 
 const Error: React.FC<ErrorProps> = ({ error }) => {
-  const refreshHandler = () => {
-    window.location.reload();
-  };
+	const refreshHandler = () => {
+		window.location.reload()
+	}
 
-  return error ? (
-    <div className="vp-error">
-      <p>
-        {error.code ? `${error.code}: ` : ''}
-        {error.message || 'Error occurred! Please try again'}
-      </p>
-      <ReloadIcon onClick={refreshHandler} />
-    </div>
-  ) : null;
-};
+	return error ? (
+		<div className='vp-error'>
+			<p>
+				{error.code ? `${error.code}: ` : ''}
+				{error.message || 'Error occurred! Please try again'}
+			</p>
+			<button onClick={refreshHandler}>
+				<ReloadIcon />
+			</button>
+		</div>
+	) : null
+}
 
-export default memo(Error);
+export default memo(Error)
--- src/hooks/timer-hook.ts
+++ src/hooks/timer-hook.ts
@@ -1,26 +1,25 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react'
 
-export const useTimeout = (): [
-  (callback: () => void, delay: number) => void,
-  () => void
-] => {
-  const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
+export const useTimeout = (): [(callback: () => void, delay: number) => void, () => void] => {
+	const timeoutRef = useRef<ReturnType<typeof setTimeout>>()
 
-  const clear = useCallback(() => {
-    timeoutRef.current && clearTimeout(timeoutRef.current);
-  }, []);
+	const clear = useCallback(() => {
+		if (timeoutRef.current) {
+			clearTimeout(timeoutRef.current)
+		}
+	}, [])
 
-  const set = useCallback(
-    (callback: () => void, delay: number) => {
-      clear();
-      timeoutRef.current = setTimeout(callback, delay);
-    },
-    [clear]
-  );
+	const set = useCallback(
+		(callback: () => void, delay: number) => {
+			clear()
+			timeoutRef.current = setTimeout(callback, delay)
+		},
+		[clear]
+	)
 
-  useEffect(() => {
-    return clear;
-  }, [clear]);
+	useEffect(() => {
+		return clear
+	}, [clear])
 
-  return [set, clear];
-};
+	return [set, clear]
+}
