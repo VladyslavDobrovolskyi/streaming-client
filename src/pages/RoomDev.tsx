e326dc25de01b58baddddc111fee17e07a80a2a3
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-01-31 14:54:30 +0200

    sadsadsa

--- src/pages/RoomDev.tsx
+++ src/pages/RoomDev.tsx
@@ -93,6 +93,7 @@ export default function RoomDev() {
 			onMouseMove={showControlsHandler}
 			onMouseLeave={() => setShowControls(false)}
 			style={{
+				backgroundColor: isPlaying ? '#333' : '#000',
 				width: '100%',
 				height: '100%',
 				position: 'relative',
