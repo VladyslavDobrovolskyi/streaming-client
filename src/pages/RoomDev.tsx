144b962cebcf8aec8ed7170936ce23e86190f9b7
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-02-01 13:08:21 +0200

    Refactor RoomDev component to streamline Slider implementation for seek control

--- src/pages/RoomDev.tsx
+++ src/pages/RoomDev.tsx
@@ -161,18 +161,17 @@ export default function RoomDev() {
 				>
 					{isPlaying ? <PauseIcon /> : <PlayIcon />}
 				</button>
-				<label style={{ margin: '0.5rem', color: '#fff', flex: 1 }}>
-					Seek
-					<Slider
-						min={0}
-						max={playerRef.current?.getDuration() || 1}
-						step={0.01}
-						value={[seekTime !== null ? seekTime : played * (playerRef.current?.getDuration() || 1)]}
-						onValueChange={handleSeekChange}
-						onMouseUp={handleSeekMouseUp}
-						style={{ width: '100%' }}
-					/>
-				</label>
+
+				<Slider
+					min={0}
+					max={playerRef.current?.getDuration() || 1}
+					step={0.01}
+					value={[seekTime !== null ? seekTime : played * (playerRef.current?.getDuration() || 1)]}
+					onValueChange={handleSeekChange}
+					onMouseUp={handleSeekMouseUp}
+					style={{ width: '100%' }}
+				/>
+
 				<div
 					style={{
 						position: 'relative',
