151b9e8d6639e864b2e342240997fd396795dd52
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-02-01 16:20:48 +0200

    Update time display font size and styling in RoomDev component for better visibility

--- src/pages/RoomDev.tsx
+++ src/pages/RoomDev.tsx
@@ -318,7 +318,7 @@ export default function RoomDev() {
 					</div>
 
 					{/* Center time display */}
-					<Text size='2'>
+					<Text size='4' align='center' color='gray'>
 						{formatTime(played * duration)} / {formatTime(duration)}
 					</Text>
 
