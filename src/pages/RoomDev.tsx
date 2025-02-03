1a16ac6fe7dbe9196fb0e2aeb8b65b8118c8abdd
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-02-03 21:40:09 +0200

    Decrease z-index for menu to enhance layout consistency in RoomDev

--- src/pages/RoomDev.tsx
+++ src/pages/RoomDev.tsx
@@ -982,7 +982,7 @@ export default function RoomDev() {
 									}}
 									onMouseLeave={handleMenuClose}
 									style={{
-										zIndex: 5000,
+										zIndex: 1000,
 										minWidth: 220,
 										backgroundColor: 'rgba(0, 0, 0, 0.8)',
 										borderRadius: 6,
