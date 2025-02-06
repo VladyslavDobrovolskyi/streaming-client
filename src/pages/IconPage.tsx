8ac2f7283023c23b0a757ca6a4cdb1fe4885e637
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-02-06 12:19:25 +0200

    Refactor IconPage to use Flex layout for Avatar components and update fallback text

--- src/pages/IconPage.tsx
+++ src/pages/IconPage.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Avatar } from '@radix-ui/themes'
+import { Avatar, Flex } from '@radix-ui/themes'
 
 const IconPage: React.FC = () => {
 	return (
 		<div>
 			<h1>Icon Page</h1>
-
-			<Avatar
-				src='https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u1f9d0/u1f9d0_u1f633.png'
-				fallback='Allure'
-			/>
+			<Flex gap='2'>
+				<Avatar
+					src='https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u1f9d0/u1f9d0_u1f633.png'
+					fallback='A'
+				/>
+				<Avatar fallback='Allure' />
+			</Flex>
 		</div>
 	)
 }
