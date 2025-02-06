064fd50bd742958af8904da5dcda810fe9d8fbb1
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-02-06 12:17:34 +0200

    Refactor IconPage to remove Flex layout and simplify Avatar component usage

--- src/pages/IconPage.tsx
+++ src/pages/IconPage.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Avatar, Flex } from '@radix-ui/themes'
+import { Avatar } from '@radix-ui/themes'
 
 const IconPage: React.FC = () => {
 	return (
 		<div>
 			<h1>Icon Page</h1>
-			<Flex gap='2'>
-				<Avatar
-					src='https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u1f9d0/u1f9d0_u1f633.png'
-					fallback='A'
-				/>
-				<Avatar fallback='A' />
-			</Flex>
+
+			<Avatar
+				src='https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u1f9d0/u1f9d0_u1f633.png'
+				fallback='A'
+			/>
 		</div>
 	)
 }
