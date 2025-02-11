641a3259d84691b0cb40575d3cf88d02d8586f85
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-02-11 11:56:02 +0200

    Enhance audio and video track toggling by stopping tracks on mute and reinitializing video stream

--- src/pages/RoomDev.tsx
+++ src/pages/RoomDev.tsx
@@ -496,10 +496,12 @@ export default function RoomDev() {
 			try {
 				if (audioTrack.enabled) {
 					audioTrack.enabled = false
+					audioTrack.stop()
 					setMicMuted(true)
 					emitInfoSync(localUsername, avatar, isCameraDisabled, true)
 				} else {
 					audioTrack.enabled = true
+					navigator.mediaDevices.getUserMedia({ video: true })
 					setMicMuted(false)
 					emitInfoSync(localUsername, avatar, isCameraDisabled, false)
 				}
@@ -516,9 +518,11 @@ export default function RoomDev() {
 			try {
 				if (videoTrack.enabled) {
 					videoTrack.enabled = false
+					videoTrack.stop()
 					setCameraMuted(true)
 					emitInfoSync(localUsername, avatar, true, isMicrophoneDisabled)
 				} else {
+					navigator.mediaDevices.getUserMedia({ video: true })
 					videoTrack.enabled = true
 					setCameraMuted(false)
 					emitInfoSync(localUsername, avatar, false, isMicrophoneDisabled)
