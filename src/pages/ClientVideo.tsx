a10a336104f20fe1f1b75b10ec50890cdcd6622b
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-02-16 17:13:21 +0200

    Adjust volume control width in ClientVideo component for improved layout

--- src/pages/ClientVideo.tsx
+++ src/pages/ClientVideo.tsx
@@ -226,7 +226,7 @@ export default function ClientVideo({
 													onValueChange={value => handleVolumeChange(value[0])}
 													style={
 														{
-															width: size.width - 35,
+															width: size.width - 45,
 															'--slider-thumb-size': '12px',
 														} as React.CSSProperties
 													}
