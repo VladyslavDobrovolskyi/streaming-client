62a20b717bb37f31b9231c803ace9fdec68e4979
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-02-16 17:48:21 +0200

    Adjust client position calculation in RoomDev component for improved layout

--- src/pages/RoomDev.tsx
+++ src/pages/RoomDev.tsx
@@ -530,7 +530,7 @@ export default function RoomDev() {
 							isMicrophoneMuted={
 								clientID === LOCAL_VIDEO ? isMicrophoneDisabled : participantData.isMicrophoneDisabled
 							}
-							position={clientPositions[clientID] || { x: 10 * index, y: 10 }}
+							position={clientPositions[clientID] || { x: 10 * index * 150, y: 10 }}
 							size={clientSizes[clientID] || { width: 150, height: 100 }}
 							onPositionChange={(id, pos) => setClientPositions(prev => ({ ...prev, [id]: pos }))}
 							onSizeChange={(id, size) => setClientSizes(prev => ({ ...prev, [id]: size }))}
