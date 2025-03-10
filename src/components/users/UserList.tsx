9d616c8effd6a6e7dee102609a8a8b307c354225
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-10 13:26:24 +0200

    Update UserList component to reverse opacity logic for private chats

--- src/components/users/UserList.tsx
+++ src/components/users/UserList.tsx
@@ -181,7 +181,7 @@ export default function UserList({
 												color: 'white',
 												padding: '5px',
 												position: 'relative',
-												opacity: privateChats[clientID] ? 0.5 : 1,
+												opacity: privateChats[clientID] ? 1 : 0.5,
 											}}
 										>
 											{unreadMessages[clientID] > 0 ? (
