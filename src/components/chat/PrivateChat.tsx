ed084d8fec8f4319fe7942ed605e6ebd2c6741e5
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-10 11:49:03 +0200

    Update background color in PrivateChat component for improved visibility

--- src/components/chat/PrivateChat.tsx
+++ src/components/chat/PrivateChat.tsx
@@ -103,7 +103,7 @@ const PrivateChat: React.FC<PrivateChatProps> = ({
 							borderBottom: '1px solid var(--gray-4)',
 							cursor: isDragging ? 'grabbing' : 'move',
 							backgroundColor: highlight
-								? 'rgba(0, 255, 255, 0.2)'
+								? 'var(--blue-9)'
 								: isActive
 								? 'var(--gray-2)'
 								: 'rgba(245, 245, 245, 0.9)',
