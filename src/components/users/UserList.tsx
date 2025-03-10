039ef02458cd76444fcda290560280c0e2d1299b
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-10 20:26:15 +0200

    Refactor UserList component header for improved styling and clarity

--- src/components/users/UserList.tsx
+++ src/components/users/UserList.tsx
@@ -73,9 +73,9 @@ export default function UserList({
 						transition: 'right 0.5s ease-in-out',
 					}}
 				>
-					{/* <h2 style={{ color: 'white', padding: '10px', borderBottom: '1px solid rgba(255, 255, 255, 0.2)' }}>
-						<FaUsers />
-					</h2> */}
+					<h2
+						style={{ color: 'white', padding: '5px', borderBottom: '1px solid rgba(255, 255, 255, 0.2)' }}
+					></h2>
 					{Object.keys(participantInfo).length === 0 && (
 						<div style={{ padding: '10px', color: 'white', textAlign: 'center' }}>
 							<p>No other participants are currently in the room.</p>
