fe84ba06a2029ad9b2eb0e644f743c3fbe0a8a89
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-10 12:37:36 +0200

    Update RoomChat component styles for improved sender message visibility and border definition

--- src/components/chat/RoomChat.tsx
+++ src/components/chat/RoomChat.tsx
@@ -189,9 +189,14 @@ const RoomChat: React.FC<RoomChatProps> = ({
 											style={{
 												display: 'inline-block',
 												backgroundColor:
-													msg.sender === realClientID ? 'transparent' : 'var(--gray-3)',
+													msg.sender === realClientID
+														? 'rgba(65, 147, 247, 0.7)'
+														: 'var(--gray-3)',
 												color: msg.sender === realClientID ? 'white' : 'var(--gray-12)',
-												border: '3px solid rgba(0, 0, 0, 0.8)',
+												border:
+													msg.sender === realClientID
+														? '3px solid rgba(0, 0, 0, 0.8)'
+														: 'none',
 												borderRadius:
 													msg.sender === realClientID
 														? getMessageClasses(msg, index).includes('message-last')
