4c6ad5f081954c1e281877bc85da9d03162f9f98
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-10 11:38:48 +0200

    Fix icon color in RoomChat component to always display as black for consistency

--- src/components/chat/RoomChat.tsx
+++ src/components/chat/RoomChat.tsx
@@ -256,7 +256,7 @@ const RoomChat: React.FC<RoomChatProps> = ({
 							<MdKeyboardReturn
 								size={18}
 								style={{
-									color: chatInput.trim() ? 'black' : 'rgba(0, 255, 255, 0.2)',
+									color: 'black',
 									opacity: '0.8',
 								}}
 							/>
