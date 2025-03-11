4dbb9c1ca7d7da780ea2cb8d2ad132a8318d2bd9
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-11 02:13:38 +0200

    Add mouse over effect to scale transformation in ClientVideo component

--- src/components/users/ClientVideo.tsx
+++ src/components/users/ClientVideo.tsx
@@ -344,6 +344,7 @@ export default function ClientVideo({
 												display: 'flex',
 												alignItems: 'center',
 											}}
+											onMouseOver={e => (e.currentTarget.style.transform = 'scale(1)')}
 											onMouseOut={e => (e.currentTarget.style.transform = 'scale(1)')}
 											onMouseDown={e => (e.currentTarget.style.transform = 'scale(0.9)')}
 											onMouseUp={e => (e.currentTarget.style.transform = 'scale(1.2)')}
