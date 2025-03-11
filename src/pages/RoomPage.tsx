06f9a0f22545766e345bf4d84a093a57c2f62a7d
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-11 19:31:48 +0200

    Add click event listener for video player to toggle playback in RoomPage

--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -692,6 +692,19 @@ export default function RoomPage() {
 			handlePause()
 		}
 	}
+
+	useEffect(() => {
+		const playerElement = document.querySelector('.react-player')
+		if (playerElement) {
+			playerElement.addEventListener('click', togglePlayByMouse)
+		}
+
+		return () => {
+			if (playerElement) {
+				playerElement.removeEventListener('click', togglePlayByMouse)
+			}
+		}
+	}, [])
 	// Add this effect to track new room chat messages
 	useEffect(() => {
 		// Only count unread messages when the chat is closed
@@ -780,7 +793,6 @@ export default function RoomPage() {
 			ref={playerWrapperRef}
 			onMouseEnter={() => setHighlightedUser(null)}
 			className={`player-wrapper ${isPlaying ? 'playing' : ''}`}
-			onClick={togglePlayByMouse}
 			onMouseMove={showControlsHandler}
 			onMouseLeave={() => {
 				if (!isDragging && !isMenuOpen) {
