591357a730c06d00437b2d6afe04a7f98b03dd67
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-12 15:18:33 +0200

    Prevent default behavior for keydown events in RoomPage

--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -422,9 +422,8 @@ export default function RoomPage() {
 		const allChatsIsClosed = !isAnyChatOpen
 
 		const handleKeyDown = (e: KeyboardEvent) => {
+			e.preventDefault()
 			if (isLoading) return
-			if (isMenuOpen) return
-
 			if (allChatsIsClosed) {
 				if (e.code === 'Space') {
 					e.preventDefault()
