6a14f7b6b57f6d41ad6e7760eba0e0261d8f7d41
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-03-12 13:56:34 +0200

    Refactor button interactions in UserList to enhance scaling effects and improve transition handling

--- src/components/users/UserList.tsx
+++ src/components/users/UserList.tsx
@@ -83,12 +83,14 @@ export default function UserList({
 							cursor: 'pointer',
 							transition: 'transform 0.2s cubic-bezier(0.4, 0, 0.2, 1)',
 						}}
-						onMouseOver={e => (e.currentTarget.style.transform = 'scale(1.2)')}
-						onMouseOut={e => (e.currentTarget.style.transform = 'scale(1)')}
-						onMouseDown={e => (e.currentTarget.style.transform = 'scale(0.9)')}
-						onMouseUp={e => (e.currentTarget.style.transform = 'scale(1.2)')}
 					>
-						<LiaUsersCogSolid style={{ color: 'white' }} />
+						<LiaUsersCogSolid
+							style={{ color: 'white' }}
+							onMouseOver={e => (e.currentTarget.style.transform = 'scale(1.2)')}
+							onMouseOut={e => (e.currentTarget.style.transform = 'scale(1)')}
+							onMouseDown={e => (e.currentTarget.style.transform = 'scale(0.9)')}
+							onMouseUp={e => (e.currentTarget.style.transform = 'scale(1.2)')}
+						/>
 					</button>
 				</div>
 			)}
