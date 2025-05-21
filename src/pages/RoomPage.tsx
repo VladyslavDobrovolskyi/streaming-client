7fb4f5efcf53849faf809fb9bb79e15887507468
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-21 23:23:26 +0300

    Remove Loader component return for unauthenticated users in RoomPage

--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -1034,11 +1034,7 @@ export default function RoomPage() {
 
 	// Check if the user is authenticated before rendering the player
 	if (!isAuthenticated) {
-		return (
-			<div style={{ backgroundColor: 'rgb(26,26,26)' }}>
-				<Loader color='#4a90e2' />
-			</div>
-		)
+		return null
 	}
 
 	return (
