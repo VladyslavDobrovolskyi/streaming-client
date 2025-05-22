3703f914478460763a391dffe6c031ed2e7939cc
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-05-22 18:32:45 +0300

    Connection fix attempt

--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -114,6 +114,11 @@ export default function RoomPage() {
 		updateUserCameraOpacity,
 	} = useLocalStorageSync(roomID!)
 
+	if (typeof window !== 'undefined' && window.performance && window.performance.navigation.type === 1) {
+		if (window.location.pathname === `/join/${roomID}`) {
+			window.location.reload()
+		}
+	}
 	// Add handlers for video loading states
 	const handleReady = () => {
 		setIsLoading(false)
